0e8634a73c8c082c801e70ac86f33793a9b874d0
femartinez974-coder <femartinez974@uan.edu.co>
2026-09-17 09:43:41 -0500

    Create 1-3-6.cpp

---
+++ 1-3-6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    int segundos, minutos;
+    cout<<"ingrese el numero para convertir de segundos a minutos"<<endl;
+    cin>>segundos;
+    minutos=(segundos/60);
+    cout<<minutos<<" minutos"<<endl;
+}
