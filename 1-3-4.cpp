fcf13cc66e51e09a1f16f6809a501ecb66be82aa
femartinez974-coder <femartinez974@uan.edu.co>
2026-09-17 09:41:14 -0500

    Create 1-3-4.cpp

---
+++ 1-3-4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main(){
+    float Numero_Entero;
+    cout<<"Digite un numero entero"<<endl;
+    cin>>Numero_Entero;
+    Numero_Entero=Numero_Entero*2;
+    cout<<Numero_Entero;
+}
