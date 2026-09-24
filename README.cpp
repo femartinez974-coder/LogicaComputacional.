4581155ab745cc9bb752359f953a6732dc39d855
femartinez974-coder <femartinez974@uan.edu.co>
2026-09-24 09:48:41 -0500

    1-3-12.cpp

---
+++ README.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int entero=100;
+    float decimal=100.01;
+    char caracter= 'C';
+    
+    cout<<"Ejemplo de variable tipo entero: "<<entero<<endl;
+    cout<<"Ejemplo de variable tipo decimal: "<<decimal<<endl;
+    cout<<"Ejemplo de variable tipo carater: "<<caracter<<endl;
+}
